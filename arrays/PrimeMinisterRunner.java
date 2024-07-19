fa9e4e69d27c04a5b4f0ce1eda606bb7a2b5d6ef
Ajaykumar-Deshpande <ajaydeshpandexworkz@gmail.com>
2024-07-19 10:00:53 +0530

    inital1

---
+++ arrays/PrimeMinisterRunner.java
@@ -0,0 +1,12 @@
+class PrimeMinisterRunner
+{
+	public static void main(String[] args)
+	{
+		System.out.println("running main in PrimeMinisterRunner");
+		String[] ministersname={"Jawaharlal Nehru","Gulzarilal Nanda","Lal Bahadur Shastri","Indira Gandhi","Morarji Desai","Charan Singh","Rajiv Gandhi","Vishwanath Pratap Singh","Chandra Shekhar","P. V. Narasimha Rao","Atal Bihari Vajpayee","H. D. Deve Gowda","I. K. Gujral","Manmohan Singh","Narendra Modi"};
+	PrimeMinister.ministers(ministersname);
+	}
+	
+}
+
+
